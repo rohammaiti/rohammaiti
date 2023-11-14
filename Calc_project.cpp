9a96923526f7dab2b8289a3d1e5ac9fb478621a0
rohammaiti <95322992+rohammaiti@users.noreply.github.com>
2023-11-14 12:53:01 +0530

    Update Calc_project.cpp

--- Calc_project.cpp
+++ Calc_project.cpp
@@ -5,104 +5,116 @@ using namespace std;
 
 class Calculator 
 {
-public:
-    double addNumber(const double numbers[], int n) 
-    {
-        double sum = 0.0;
-        for (int i = 0; i < n; ++i) 
-        {
-            sum += numbers[i];
+    public:
+        double addNumber(const double numbers[], int n) {
+            double sum = 0.0;
+            for (int i = 0; i < n; ++i) {
+                sum += numbers[i];
+            }
+            return sum;
         }
-        return sum;
-    }
 
-    double multiplyNumber(const double numbers[], int n) 
-    {
-        double product = 1.0;
-        for (int i = 0; i < n; ++i) 
-        {
-            product *= numbers[i];
+        double multiplynumber(const double numbers[], int n) {
+            double product = 1.0;
+            for (int i = 0; i < n; ++i) {
+                product *= numbers[i];
+            }
+            return product;
         }
-        return product;
-    }
-
-    double subtract(double a, double b) 
-    {
-        return a - b;
-    }
-
-    double divide(double Dividend, double Divisor) 
-    {
-        return Dividend / Divisor;
-    }
 };
 
 class SciCalculator : public Calculator 
 {
-public:
-    double Percentage(double value, double percentage) 
-    {
-        return (value * percentage) / 100.0;
-    }
-
-    int Factorial(int n) 
-    {
-        if (n == 0 || n == 1) 
+    public:
+        double Percentage(double value, double percentage) 
         {
-            return 1;
-        } 
-        else 
-        {
-            return n * Factorial(n - 1);
+            return (value * percentage) / 100.0;
         }
-    }
 
-    double Square(double num) 
-    {
-        return num * num;
-    }
+        int Factorial(int n) 
+        {
+            if (n == 0 || n == 1) 
+            {
+                return 1;
+            } 
+            
+            else 
+            {
+                return n * Factorial(n - 1);
+            }
+        }
 
-    double SquareRoot(double num) 
-    {
-        return sqrt(num);
-    }
+        double Square(double num) 
+        {
+            return num * num;
+        }
+
+        double SquareRoot(double num) 
+        {
+            return sqrt(num);
+        }
+
+        double calculatePower(double x, double y) 
+        {
+            return pow(x, y);
+        }
+
+        double subtract(double a, double b) 
+        {
+            return a - b;
+        }
+
+        double divide(double dividend, double divisor) 
+        {
+            if (divisor != 0) 
+            {
+                return dividend / divisor;
+            } 
+            
+            else 
+            {
+                cout << "Error: Division by zero is undefined." << endl;
+                return NAN;
+            }
+        }
 };
 
 int main() 
 {
     SciCalculator sciCalculator;
-
     int choice;
 
-    do {
-        cout << "Select operation:"<<endl
-             << "1. Addition"<<endl
-             << "2. Subtraction"<<endl
-             << "3. Multiplication"<<endl
-             << "4. Division"<<endl
-             << "5. Percentage"<<endl
-             << "6. Factorial"<<endl
-             << "7. Square"<<endl
-             << "8. Square Root"<<endl
-             << "0. Exit"<<endl
-             << "Enter choice (0-8): ";
+    do 
+    {
+        cout << "Select operation: \n"
+             << "1. Addition\n"
+             << "2. Multiplication\n"
+             << "3. Subtraction\n"
+             << "4. Division\n"
+             << "5. Percentage\n"
+             << "6. Factorial\n"
+             << "7. Square\n"
+             << "8. Square Root\n"
+             << "9. Power\n"
+             << "0. Exit\n"
+             << "Enter choice (0-9): ";
         cin >> choice;
 
         if (choice == 0) 
         {
             cout << "Exiting..." << endl;
-            break; 
+            break;
         }
 
-        if (choice >= 1 && choice <= 4) 
+        if (choice == 1 || choice == 2) 
         {
             int size;
-            cout << "Enter the number of elements- ";
+            cout << "Enter the number of elements: ";
             cin >> size;
 
             double *numbers = new double[size];
 
-            cout << "Enter the elements-" << endl;
+            cout << "Enter the elements, separated by spaces:" << endl;
             for (int i = 0; i < size; ++i) 
             {
                 cin >> numbers[i];
@@ -112,37 +124,46 @@ int main()
             {
                 double result = sciCalculator.addNumber(numbers, size);
                 cout << "Sum: " << result << endl;
-            }
-
+            } 
+            
             else if (choice == 2) 
             {
-                double result = sciCalculator.subtract(numbers[0], numbers[1]);
-                cout << "Difference: " << result << endl;
+                double result = sciCalculator.multiplynumber(numbers, size);
+                cout << "Product: " << result << endl;
             }
 
-            else if (choice == 3) 
+            delete[] numbers;
+        } 
+        
+        else if (choice == 3 || choice == 4) 
+        {
+            double num1, num2;
+            cout << "Enter the first number: ";
+            cin >> num1;
+            cout << "Enter the second number: ";
+            cin >> num2;
+
+            if (choice == 3) 
             {
-                double result = sciCalculator.multiplyNumber(numbers, size);
-                cout << "Product: " << result << endl;
+                double result = sciCalculator.subtract(num1, num2);
+                cout << "Difference: " << result << endl;
             } 
             
             else if (choice == 4) 
             {
-                if (numbers[1] != 0) 
+                if (num2 != 0) 
                 {
-                    double result = sciCalculator.divide(numbers[0], numbers[1]);
+                    double result = sciCalculator.divide(num1, num2);
                     cout << "Quotient: " << result << endl;
                 } 
-                else 
-                {
-                    cout << "Error: Division by zero" << endl;
+                
+                else {
+                    cout << "Error: Division by zero is undefined." << endl;
                 }
             }
-
-            delete[] numbers;
-
         } 
-        else if (choice >= 5 && choice <= 8) 
+        
+        else if (choice >= 5 && choice <= 9) 
         {
             double num;
             cout << "Enter a number: ";
@@ -176,18 +197,29 @@ int main()
                     double result = sciCalculator.SquareRoot(num);
                     cout << "Square Root: " << result << endl;
                 } 
+                
                 else 
                 {
                     cout << "Error: Cannot calculate square root of a negative number." << endl;
                 }
+            } 
+            
+            else if (choice == 9) 
+            {
+                double exponent;
+                cout << "Enter the exponent for power calculation: ";
+                cin >> exponent;
+                double result = sciCalculator.calculatePower(num, exponent);
+                cout << num << " ^ " << exponent << " = " << result << endl;
             }
         } 
         
         else 
         {
-            cout << "Invalid choice. Please select 0 to 8." << endl;
+            cout << "Invalid choice. Please select 0 to 9." << endl;
         }
-    } 
-    while (choice != 0);
+
+    } while (choice != 0);
+
     return 0;
 }
